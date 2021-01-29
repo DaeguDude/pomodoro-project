18d3d0028cbbaadc692c312437997fda7478a2d5
Daegudude <k3hppk@gmail.com>
2021-01-29 17:01:32 +0900

    Implemented render function to create task dynamically

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 // 페이지가 로딩이 되면, Task Section을 화면에 띄우고 클릭 이벤트에 등록을 하는 등 초기 세팅을 하자.
 import "./reset.css";
 import "./task.css";
+import Task from './task';
 import { CreateTask, EditTask } from './taskDetail';
 import AddTaskBtn from "./addTaskBtn";
 
-
+const myTask = new Task();
 const myAddTaskBtn = new AddTaskBtn();
 const myCreateTask = new CreateTask();
 const myEditTask = new EditTask();
 
+console.log(myTask.render(0, 0, 0));
+
 const taskSection = document.querySelector('#task-section');
+const taskList = document.querySelector('.task-list');
+taskList.appendChild(myTask.render('It is happneing', 0, 10));
+taskList.appendChild(myTask.render('It is happneinghappneinghappneinghappneinghappneinghappneinghappneinghappneinghappneing', 0, 10));
+taskList.appendChild(myTask.render('SHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPPSHUTUPP', 0, 10));
+taskList.appendChild(myTask.render('It is happneing', 0, 10));
+
 
 const addTaskBtn = document.querySelector(".add-task-btn");
 addTaskBtn.addEventListener("click", () => {
@@ -19,24 +28,3 @@ addTaskBtn.addEventListener("click", () => {
 
 });
 
-
-// const taskDetail = document.querySelector(".task-detail");
-// taskDetail.remove();
-
-// taskSection.addEventListener("click", (e) => {
-//   if (e.target.className.includes("task__check-marker")) {
-//     console.log("check marker");
-//   }
-
-//   if (
-//     e.target.className.includes("edit-btn") ||
-//     e.target.className.includes("fa-ellipsis-v")
-//   ) {
-//     console.log("edit-btn clicked");
-//   }
-
-//   if (e.target.className.includes("add-task-btn")) {
-//     console.log("add task btn clicked");
-//   }
-//   console.log(e.target);
-// });
--- src/task.js
+++ src/task.js
@@ -1,13 +1,13 @@
 export default class Task {
-  constructor(TaskInfo) {
-    const { id, title, note, estPomodoros } = TaskInfo;
-    this.id = id;
-    this.title = title;
-    this.note = note;
-    this.estPomodoros = estPomodoros;
-    this.completedPomodoros = 0;
-    this.isCompleted = false;
-  }
+  // constructor(TaskInfo) {
+  //   const { id, title, note, estPomodoros } = TaskInfo;
+  //   this.id = id;
+  //   this.title = title;
+  //   this.note = note;
+  //   this.estPomodoros = estPomodoros;
+  //   this.completedPomodoros = 0;
+  //   this.isCompleted = false;
+  // }
 
   toggleCompleted() {
     this.isCompleted = !this.isCompleted;
@@ -27,5 +27,24 @@ export default class Task {
 
   incrementCompletedPomodoros() {
     this.completedPomodoros++;
+  }
+
+  render(title, completedPomodoros, estimatedPomodoros) {
+    return document.createRange().createContextualFragment
+      (`
+        <li class="task">
+        <i class="task__check-marker fas fa-check-circle"></i>
+        <span class="task__title">
+          ${title}
+        </span>
+        <div class="task__pomodoros">
+          <span class="task__completed-pomodoros">${completedPomodoros}</span> /
+          <span class="task__estimated-pomodoros">${estimatedPomodoros}</span>
+        </div>
+        <button class="edit-btn">
+          <i class="fas fa-ellipsis-v"></i>
+        </button>
+      </li>
+    `);
   }
 }
