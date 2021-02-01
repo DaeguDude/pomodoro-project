5120e5aaacb3903c735c5037185281c90d2c9740
Jeong-hwan Choi <jeonghwan@Jeong-hwans-iMac.local>
2021-02-01 21:49:11 +0900

    Added webpack

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { sayHello } from "./dropdown.js";
+import '../style/newstyle.css';
+import Icon from '../img/icon-white.png';
+
+const logo = document.querySelector('.logo')
+
+// Add the image to our existing div.
+const myIcon = new Image();
+myIcon.src = Icon;
+
+logo.appendChild(myIcon);
+
+
+
+console.log('hello');
+
+sayHello();
+
+
+navBar.render();
