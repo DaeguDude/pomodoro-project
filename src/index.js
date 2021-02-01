7eb2d3dc0d483c2ab559e0417c4f3c39d40a692a
Jeong-hwan Choi <jeonghwan@Jeong-hwans-iMac.local>
2021-02-01 23:34:35 +0900

    Added url-loader and file-loader

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import { sayHello } from "./dropdown.js";
 import '../style/style.css';
-import Icon from '../img/icon-white.png';
+import IconWhite from './img/icon-white.png';
+// import Report from './img/graph-white.png';
+// import Setting from './img/config-white.png';
+// import
+
 
 const logo = document.querySelector('.logo')
 
 // Add the image to our existing div.
-const myIcon = new Image();
-myIcon.src = Icon;
-
-logo.appendChild(myIcon);
-
-
-
-console.log('hello');
-
-sayHello();
-
+const serviceIcon = new Image();
+serviceIcon.src = IconWhite;
+logo.appendChild(serviceIcon);
 
 navBar.render();
