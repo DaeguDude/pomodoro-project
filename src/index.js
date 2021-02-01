f3535bad277e37f4297d8cc903a4becfa123ae1f
Daegudude <k3hppk@gmail.com>
2021-02-01 15:55:38 +0900

    Completed pesudocode for task detail

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // 페이지가 로딩이 되면, Task Section을 화면에 띄우고 클릭 이벤트에 등록을 하는 등 초기 세팅을 하자.
 import "./reset.css";
 import "./task.css";
-import Task from './task';
-import { CreateTask, EditTask } from './taskDetail';
+import Task from "./task";
+import { CreateTask, EditTask } from "./taskDetail";
 import AddTaskBtn from "./addTaskBtn";
 
 const myTask = new Task();
@@ -10,16 +10,18 @@ const myAddTaskBtn = new AddTaskBtn();
 const myCreateTask = new CreateTask();
 const myEditTask = new EditTask();
 
-const taskSection = document.querySelector('#task-section');
-const taskList = document.querySelector('.task-list');
-
+const taskSection = document.querySelector("#task-section");
+const taskList = document.querySelector(".task-list");
+taskList.appendChild(myTask.render("아 잠온다", 0, 10));
+taskList.appendChild(myTask.render("아 잠온다", 0, 10));
+taskList.appendChild(myTask.render("아 잠온다", 0, 10));
+taskList.appendChild(myTask.render("아 잠온다", 0, 10));
+taskList.appendChild(myTask.render("아 잠온다", 0, 10));
+taskList.appendChild(myTask.render("아 잠온다", 0, 10));
+taskList.appendChild(myTask.render("아 잠온다", 0, 10));
+taskList.appendChild(myTask.render("아 잠온다", 0, 10));
 
 const addTaskBtn = document.querySelector(".add-task-btn");
 addTaskBtn.addEventListener("click", () => {
-  
-  taskSection.appendChild(myEditTask.render())
-
-
+  taskSection.appendChild(myEditTask.render());
 });
-
-
