b564791c36909ba7c611910fd839a61abf0e6cce
Jeong-hwan Choi <jeonghwan@Jeong-hwans-iMac.local>
2021-02-01 23:45:48 +0900

    Changed webpack config to use file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,11 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ]
       },
     ],
   },
