01a42f0498e72a66c57a59366696899f2d9bc7e3
Sanghak <k3hppk@gmail.com>
2021-02-02 12:18:08 +0900

    edit task created

---
+++ src/EditTask/editTask.js
@@ -0,0 +1,145 @@
+const editTaskHtmlContent = `
+    <article class="task-detail edit-task">
+    <main>
+      <div class="task-detail__title">
+        <input
+          name="task-detail__title"
+          type="text"
+          placeholder="What are you working on?"
+        />
+      </div>
+      <div class="task-detail__pomodoro">
+        <span class="label">Est Pomodoros</span>
+        <div class="task-detail__pomodoro-content">
+          <input name="number-pomodoros" type="number" value="1" />
+          <button class="pomodoro-updown-btn pomodoro-up" data-action="increment">
+            <i class="fas fa-caret-up"></i>
+          </button>
+          <button class="pomodoro-updown-btn pomodoro-down" data-action="decrement">
+            <i class="fas fa-caret-down"></i>
+          </button>
+        </div>
+      </div>
+      <div class="task-detail__note">
+        <button class="add-note-btn" data-action="addNote">+ Add Note</button>
+      </div>
+    </main>
+    <footer>
+      <button class="task-detail__delete-btn" data-action="delete">Delete</button>
+      <div>
+        <button class="task-detail__cancel-btn" data-action="cancel">Cancel</button>
+        <button class="task-detail__save-btn task-detail__save-btn--disabled" data-action="save">Save</button>
+      </div>
+    </footer>
+    </article>
+  `;
+
+class EditTask {
+  constructor() {}
+
+  render() {
+    const result = document
+      .createRange()
+      .createContextualFragment(editTaskHtmlContent);
+
+    result
+      .querySelector(".edit-task")
+      .addEventListener("click", this.onClickHandler.bind(this));
+
+    result
+      .querySelector(".task-detail__title")
+      .addEventListener("input", this.onInputHandler.bind(this));
+
+    return result;
+  }
+
+  hide() {
+    document.querySelector(".create-task").remove();
+  }
+
+  save() {
+    this.getTaskInfo();
+  }
+
+  increment() {
+    document.querySelector('input[name="number-pomodoros"]').value++;
+  }
+
+  decrement() {
+    document.querySelector('input[name="number-pomodoros"]').value--;
+  }
+
+  delete() {
+    console.log("Close modal")
+    console.log("Delete task")
+  }
+
+  addNote() {
+    removeAddNoteBtn();
+    showTextArea(createTextArea());
+
+    function createTextArea() {
+      return document
+        .createRange()
+        .createContextualFragment(
+          `<textarea name="note" placeholder="Some notes..."></textarea>`
+        );
+    }
+
+    function removeAddNoteBtn() {
+      document.querySelector(".add-note-btn").remove();
+    }
+
+    function showTextArea(noteTextArea) {
+      document.querySelector(".task-detail__note").appendChild(noteTextArea);
+    }
+  }
+
+  cancel() {
+    this.hide();
+  }
+
+  onClickHandler(event) {
+    let action;
+    if (event.target.tagName === "I") {
+      const pomodoroUpdownBtn = event.target.closest(".pomodoro-updown-btn");
+      action = pomodoroUpdownBtn.dataset.action;
+    } else {
+      action = event.target.dataset.action;
+    }
+
+    if (action) {
+      this[action]();
+    }
+  }
+
+  onInputHandler(event) {
+    if (event.target.value) {
+      return document
+        .querySelector(".task-detail__save-btn")
+        .classList.remove("task-detail__save-btn--disabled");
+    }
+
+    if (!event.target.value) {
+      return document
+        .querySelector(".task-detail__save-btn")
+        .classList.add("task-detail__save-btn--disabled");
+    }
+  }
+
+  getTaskInfo() {
+    const title = document.querySelector('input[name="task-detail__title"]').value
+    const numPomodoros = document.querySelector('input[name="number-pomodoros"]').value;
+    let note = '';
+    if (document.querySelector('textarea[name="note"]')) {
+      note = document.querySelector('textarea[name="note"]').value;
+    }
+
+    console.log({ title, numPomodoros, note })
+    return { title, numPomodoros, note }
+  }
+}
+
+const container = document.querySelector(".container");
+const myEditTask = new EditTask();
+container.appendChild(EditTask.prototype.render());
