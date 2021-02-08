38214a01245015ecb992065216ad3e2a3067f968
Daegudude <k3hppk@gmail.com>
2021-02-08 21:54:05 +0900

    Remove unnecessary styling from task css
    We haved moved shared css stylings into style.css, thus we don't need
    this styling in task css anymore.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import { renderHeader } from './share_navbar/navbar'
-import Setting from './setting'
-import TaskSection from './taskSection/taskSection';
+import "./styles/reset.css";
+import "./styles/style.css";
 
-import { mode } from './timer/timerSection';
-import { timer } from './timer/timerSection';
+import { renderHeader } from "./share_navbar/navbar";
+import Setting from "./setting/setting";
+import TaskSection from "./taskSection/taskSection";
+
+import { mode } from "./timer/timerSection";
+import { timer } from "./timer/timerSection";
 
 const myTaskSection = new TaskSection();
 const mySetting = new Setting();
@@ -18,7 +21,7 @@ mode.waitForMode();
 timer.waitForTimer();
 timer.listenSettings();
 
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 console.log(container);
-container.appendChild(myTaskSection.render())
-myTaskSection.start()
+container.appendChild(myTaskSection.render());
+myTaskSection.start();
